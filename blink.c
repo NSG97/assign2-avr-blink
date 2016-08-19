5c46cd7aa5f0939bea9d5a9f04f748c3c86c6432
NSG97 <nishant15151@iiitd.ac.in>
2016-08-19 15:50:03 +0530

    Update blink.c

--- blink.c
+++ blink.c
@@ -2,9 +2,9 @@
 #include<util/delay.h>
 #define F_CPU 16000000UL
 int main(void){
-	DDRB=DDRB|100000;
+	DDRB=DDRB|0x20;
 	while(1){
-			PORTB=PORTB^100000;
+			PORTB=PORTB^0x20;
 			_delay_ms(500);
 	}
 }
